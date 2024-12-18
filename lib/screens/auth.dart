74c83cf74edc9bcdaae13a7a091bffa208537f28
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2024-12-18 10:52:25 -0800

    Upload user data to firebase db

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:chat_app/widgets/user_image_picker.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -61,7 +62,15 @@ class _AuthScreenState extends State<AuthScreen> {
 
         await storateRef.putFile(_selectedImage!);
         final imgUrl = await storateRef.getDownloadURL();
-        print(imgUrl);
+
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc('userCredentials.user!.uid')
+            .set({
+          'username': 'to be done...',
+          'email': _enteredEmail,
+          'image_url': imgUrl,
+        });
       }
     } on FirebaseAuthException catch (error) {
       setState(() {
