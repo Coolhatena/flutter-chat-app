e1d6001dc35a28a051e16ea2f141592509c44645
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2024-12-19 22:49:19 -0800

    Add push notification logic to firebase backend

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,26 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const {
+	onDocumentCreated,
+  } = require("firebase-functions/v2/firestore");
+   
+  const admin = require('firebase-admin');
+   
+  admin.initializeApp();
+   
+  // Cloud Firestore triggers ref: https://firebase.google.com/docs/functions/firestore-events
+  exports.myFunction = 
+	onDocumentCreated("/chat/{messageId}", async (event) => {
+	  // Return this function's promise, so this ensures the firebase function
+	  // will keep running, until the notification is scheduled.
+	  return admin.messaging().send({
+		// Sending a notification message.
+		notification: {
+		  title: event.data.data()['username'],
+		  body: event.data.data()['text'],
+		},
+		data: {
+		  // Data payload to be sent to the device.
+		  click_action: 'FLUTTER_NOTIFICATION_CLICK',
+		},
+		topic: 'chat',
+	  });
+	});
